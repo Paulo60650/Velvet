081bc2d5392cf7ab53c7d4122948353c3bf33cec
Paul Dacheux <58972637+Paulo60650@users.noreply.github.com>
2020-05-26 13:10:14 +0200

    ajustement

--- controlers/delete_control.php
+++ controlers/delete_control.php
@@ -5,12 +5,8 @@ $disc = new Disc();
 // Récupération de l'id via la méthode get
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 // Appel de la méthode setDelDisc
-if($disc->setDelDisc($id)){
+$disc->setDelDisc($id);
     $class = 'list-group-item list-group-item-success text-center';
     $message = "Le Vynile a été supprimé avec succès !  Vous allez être redirigé vers l'accueil dans 3 sec";
     header("refresh:3;url=../index.php");
-}else{
-    $class = 'list-group-item list-group-item-danger text-center';
-    $message = "Le Vynile n'a pas pu  être supprimé !  Vous allez être redirigé vers l'accueil dans 3 sec";
-    header("refresh:3;url=../index.php");
-}
+
